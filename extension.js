f13f56be984f81b4dca4b460ec02fa773356c08a
Fedor Splitt <splitt@campus.tu-berlin.de>
2024-01-17 20:21:08 +0100

    GNOME 45 fix

--- extension.js
+++ extension.js
@@ -1,4 +1,5 @@
 import * as Main from "resource:///org/gnome/shell/ui/main.js";
+import * as WindowAttentionHandler from "resource:///org/gnome/shell/ui/windowAttentionHandler.js";
 import {
   Extension,
   gettext as _,
@@ -7,17 +8,20 @@ import {
 export default class StealMyFocus extends Extension {
   enable() {
     console.log("Enabling StealMyFocus");
+    this._init();
   }
 
   disable() {
     console.log("Disabling StealMyFocus");
+    this.destroy();
   }
 
   _init() {
-    this._tracker = Shell.WindowTracker.get_default();
+    //this._tracker = Shell.WindowTracker.get_default();
+    this._tracker = WindowAttentionHandler._tracker;
     this._handlerid = global.display.connect(
       "window-demands-attention",
-      Lang.bind(this, this._onWindowDemandsAttention)
+      this._onWindowDemandsAttention.bind(this)
     );
   }
 
